e4e1f269dcf1e92c0586873541eff6e395ece428
jingapore <ngjingsong@gmail.com>
2017-02-20 00:46:29 +0800

    successfully scrolled through pages

--- webscraper1.py
+++ webscraper1.py
@@ -1,49 +1,65 @@
-#newlinefornewcommit
 from bs4 import BeautifulSoup
-import urllib3
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import re
+import pandas as pd
 
-file = open('prettified_html', 'w')
-
-http = urllib3.PoolManager()
-page = 11
 url = str('https://www.mha.gov.sg/Newsroom/press-releases/Pages/press-releases.aspx')
-print(url)
-response = http.request('GET', url, preload_content=False)
-html = response.read()
-soup = BeautifulSoup(html, 'lxml')
+browser = webdriver.Firefox()
+browser.get(url)
+html = browser.page_source
 
+#http = urllib3.PoolManager()
+#page = 11
+#response = http.request('GET', url, preload_content=False)
+#html = response.read()
+soup = BeautifulSoup(html, 'lxml')
+file = open('htmlprettified', 'w+')
 file.write(soup.prettify())
 final_table = []
+import time
 
 def tabulate_pressreleases(html_soup):
     global final_table
-    table = soup.findAll('tr')
-    for i in table:
+
+    list_date_tags = html_soup.findAll('div', {'class' : 'date js-date show'})
+    list_title_tags = html_soup.findAll('h3')
+
+    list_date = []
+    list_title = []
+    list_link = []
+
+    for i in list_date_tags:
+        date = re.search('(?<=>)[^<]+', str(i)).group()
+        list_date.append(date)
+
+    for i in list_title_tags:
+        title = re.search('(?<=>)[^<]+', str(i)).group()
+        list_title.append(title)
+
+    for title_tag in list_title_tags:
+        link_raw = title_tag.parent
+        link = re.search('(?<=<a href=").+?(?=")', str(link_raw)).group()
+        list_link.append(link)
+
+    for i in range(len(list_date)):
         table_entry = {
-            'date': 'blank',
-            'title': 'blank',
-            'link': 'blank'
+            'date': list_date[i],
+            'title': list_title[i],
+            'link': list_link[i]
         }
-        table_entry['date'] = re.search('(?<=string;#)[^<]+', str(i)).group()
-        table_entry['title'] = re.search('(?<=">).+?(?=</a>)', str(i)).group()
-        table_entry['link'] = re.search('(?<=<a href=").+?(?=">)', str(i)).group()
         final_table.append(table_entry)
 
-for i in range(60):
-    global page
-    print(url)
-    response = http.request('GET', url, preload_content=False)
-    html = response.read()
-    soup = BeautifulSoup(html, 'lxml')
-    tabulate_pressreleases(soup)
-    page += 10
-    print(page)
-    print(final_table)
-# print(final_table)
-# print(len(final_table))
-# print(final_table)
+def tabulate_through():
+    for i in range(68):
+        global html
+        global soup
+        tabulate_pressreleases(soup)
+        print(final_table)
+        browser.find_element(By.XPATH, '//*[@id="PageLinkNext"]').click()
+        time.sleep(2)
+        html = browser.page_source
+        soup = BeautifulSoup(html, 'lxml')
 
-# print(response.status)
-# print(soup.headers)
-# print(soup.prettify())
+tabulate_through()
+df = pd.DataFrame.from_dict(final_table)
